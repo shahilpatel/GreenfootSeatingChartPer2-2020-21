b000c58d8ba44e58091fdb530033a9a2ebd1a56a
Anirudh Nagasamudra <aninaga56@gmail.com>
2020-08-07 14:04:35 -0700

    first commit

--- Classroom.java
+++ Classroom.java
@@ -2,11 +2,11 @@ import java.util.*;
 import greenfoot.*;
 import java.util.ArrayList;
  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
- 
+
 /**
  * Write a description of class Classroom here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class Classroom extends World
@@ -14,21 +14,21 @@ public class Classroom extends World
     private ArrayList<Object> listo = new ArrayList<Object>();
     /**
      * Constructor for objects of class Classroom.
-     * 
+     *
      */
     public Classroom()
-    {    
+    {
         // Create a new world with 10x6 cells with a cell size of 130x130 pixels.
-        super(10, 6, 130); 
+        super(10, 6, 130);
 
         prepare();
     }
-    
+
     /**
      * Prepare the world for the start of the program. That is: create the initial
      * objects and add them to the world.
      */
-   
+
      private void prepare()
     {
         // Add three lines to this doc with your class constructor and your row and seat number
@@ -38,9 +38,13 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout("Kilgore", "Trout", 1, 1);
         addObject(kilgoretrout, 1, 1);
         kilgoretrout.sitDown();
+
+        AnirudhNagasamudra anirudhnagasamudra = new AnirudhNagasamudra("Anirudh", "Nagasamudra", 9, 5);
+        addObject(anirudhnagasamudra, 9, 5);
+        anirudhnagasamudra.sitDown();
          
-        
 
 
-    }  
+
+    }
 }
